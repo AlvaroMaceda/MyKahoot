077c55f54bc340a0ac75a98fe9b0f9477c46f77f
Alvaro Maceda <alvaro@alvaromaceda.es>
2025-10-26 08:53:09 +0100

    First design

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h2 className="csv-label">Sube un fichero CSV:</h2>
+      <input type="file" accept=".csv" className="csv-input" />
+
+      <h3 className="existing-tests-label">Haz uno de los tests ya existentes</h3>
+      <div className="tests-list">
+        {tests.map(test => (
+          <div className="test-card" key={test.id}>
+            <span className="test-name">{test.name}</span>
+            <div className="test-actions">
+              <button className="download-btn">descargar</button>
+              <button className="delete-btn">x</button>
+            </div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+const tests = [
+  { id: 1, name: 'Test de patatas' },
+  { id: 2, name: 'Test de chorizos' },
+  { id: 3, name: 'Test de Bacalaos' },
+];
+
+export default App;
